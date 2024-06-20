8845c4712de52137ff32c34995bca94e93c483f3
Роман Шамов <deadsher@mail.ru>
2024-06-20 17:05:12 +0500

    hotfix

---
+++ server/src/server.ts
@@ -0,0 +1,7 @@
+const app = require("./app");
+const port = process.env.DB_PORT || 5000;
+
+console.log("Database synced");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
